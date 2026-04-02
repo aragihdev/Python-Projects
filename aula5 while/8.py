f2c7751dd49ccca0f6f4e3629b779f94b3803b71
RAFAEL MENEGHIN REIS <1051392611035@fatec.corp>
2026-04-02 10:44:39 -0300

    exercicios while

--- aula5 while/8.py
+++ aula5 while/8.py
@@ -2,8 +2,7 @@ print("Controle de estoque")
 option = 'ask'
 menu = 1
 while menu == 1:
-    while option == 'ask':
-        option = input("O que deseja fazer (Entrada, Saída, exibição)?").lower()
+    option = input("O que deseja fazer (Entrada, Saída, exibição)?").lower()
     notebook = 0
     desktop = 0
     monitor = 0
@@ -13,45 +12,46 @@ while menu == 1:
         print("Seção: Entrada de produtos ~")
         stay = input("Deseja seguir nesta seção? (Y/N)").lower()
         if stay == 'y':
-            pass
+            produto = input("Qual produto deseja adicionar? (notebook, desktop, monitor):")
+            add = float(input(f"Digite quantos {produto} deseja adicionar ao estoque:"))
+            if produto == 'notebook':
+                if add > 0:
+                    notebook += add
+            elif produto == 'desktop':
+                if add > 0:
+                    desktop += add
+            elif produto == 'monitor':
+                if add > 0:
+                    monitor += add
+            else:
+                print(f"{produto} não um produto válido.")
         else:
-            continue
-        produto = input("Qual produto deseja adicionar? (notebook, desktop, monitor):")
-        add = float(input(f"Digite quantos {produto} deseja adicionar ao estoque:"))
-        if produto == 'notebook':
-            if add > 0:
-                notebook += add
-                add = 0
-        elif produto == 'desktop':
-            if add > 0:
-                desktop += add
-                add = 0
-        elif produto == 'monitor':
-            if add > 0:
-                monitor += add
-                add = 0
-        else:
-            print(f"{produto} não um produto válido.")
+            print("Voltando para seção inicial...")
     elif option == 'saida':
         print("Seção: Saída de produtos ~")
-        produto = input("Qual produto deseja dar saída? (notebook, desktop, monitor")
-        saida = float(input(f"Digite quantos {produto} deseja dar saída do estoque:"))
-        if produto == 'notebook':
-            if saida > 0:
-                if saida < notebook:
-                    notebook -= add
-        elif produto == 'desktop':
-            if saida > 0:
-                if saida < desktop:
-                    desktop -= add
-        elif produto == 'monitor':
-            if add > 0:
-                if saida < monitor:
-                    monitor += add
+        stay = input("Deseja seguir nesta seção? (Y/N)").lower()
+        if stay == 'y':
+            produto = input("Qual produto deseja dar saída? (notebook, desktop, monitor")
+            saida = float(input(f"Digite quantos {produto} deseja dar saída do estoque:"))
+            if produto == 'notebook':
+                if saida > 0:
+                    if saida < notebook:
+                        notebook -= add
+            elif produto == 'desktop':
+                if saida > 0:
+                    if saida < desktop:
+                        desktop -= add
+            elif produto == 'monitor':
+                if add > 0:
+                    if saida < monitor:
+                        monitor += add
+            else:
+                print(f"{produto} não um produto válido.")
         else:
-            print(f"{produto} não um produto válido.")
+            print("Voltando para seção inicial...")
     elif option == 'exibição':
             print("Seção: Exibição")
             print(f"{notebook} unidades de notebook no estoque.")
             print(f"{desktop} unidades de desktop no estoque.")
             print(f"{monitor} unidades de monitor no estoque.")
+            print("Voltando para seção inicial...")
